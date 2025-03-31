581bf23d3953587b501a0f9a3af2e5fbeb5d169f
bob240 <74813341+bob240@users.noreply.github.com>
2025-03-31 21:32:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ cd box64
 mkdir build
 cd build
 cmake .. -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DARM64=1 -DWINLATOR_GLIBC=1 -DARM_DYNAREC=1 -DBAD_SIGNAL=1 -DCMAKE_BUILD_TYPE=Release -DBOX32=1
+make -j
 patchelf --set-interpreter /data/data/com.winlator/files/rootfs/lib/ld-linux-aarch64.so.1 ./box64
